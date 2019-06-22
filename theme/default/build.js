ed7178e613799e11f21e48d5e8a772693c41a722
Hadron67 <604700341@qq.com>
2019-06-22 20:33:51 +0800

    travis fix

--- theme/default/build.js
+++ theme/default/build.js
@@ -7,22 +7,51 @@ const fs = require('fs');
 
 const _ = p => pathd.join(__dirname, p);
 
-function terserOneFile(input, output){
-    input = _(input), output = _(output);
-    input = fs.readFileSync(input, 'utf-8');
-    input = terser.minify(input);
-    fs.writeFileSync(output, input.code);
+function readFile(p){
+    return new Promise((resolve, reject) => {
+        fs.readFile(p, (err, data) => err ? reject(err) : resolve(data.toString('utf-8')));
+    });
 }
 
-rollup({
-    input: 'js/main/main.js',
-    output: {
-        file: 'dist/main.js',
+function writeFile(p, data){
+    return new Promise((resolve, reject) => {
+        fs.writeFile(p, data, err => err ? reject(err) : resolve());
+    });
+}
+
+function exists(f){
+    return new Promise((resolve, reject) => {
+        fs.exists(f, e => resolve(e));
+    });
+}
+
+function mkdir(d){
+    return new Promise((resolve, reject) => {
+        fs.mkdir(d, err => err ? reject(err) : resolve());
+    });
+}
+
+async function terserOneFile(input, output){
+    input = _(input), output = _(output);
+    input = await readFile(input);
+    input = terser.minify(input);
+    await writeFile(output, input.code);
+}
+
+(async () => {
+    !await exists(_('dist')) && await mkdir(_('dist'));
+    
+    const bundle = await rollup({
+        input: _('js/main/main.js'),
+        plugins: [
+            terserPlugin.terser()
+        ]
+    });
+    await bundle.write({
+        file: _('dist/main.js'),
         name: 'hkm',
         format: 'umd'
-    },
-    plugins: [
-        terserPlugin.terser()
-    ]
-});
-terserOneFile('js/katex-renderer.js', 'dist/katex-renderer.js');
+    });
+
+    await terserOneFile('js/katex-renderer.js', 'dist/katex-renderer.js');
+})();
